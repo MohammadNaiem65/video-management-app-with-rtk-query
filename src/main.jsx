20c377167f39b7ec6bc4b170d8ca2d0d7723d6aa
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-10-03 08:07:03 +0600

    created the store

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App.jsx';
 import Home from './pages/Home/Home.jsx';
-import './index.css';
 import Video from './pages/Video/Video.jsx';
 import AddVideo from './pages/AddVideo/AddVideo.jsx';
+import './index.css';
+import store from './app/store.js';
 
 const routes = createBrowserRouter([
 	{
@@ -30,6 +32,8 @@ const routes = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<RouterProvider router={routes} />
+		<Provider store={store}>
+			<RouterProvider router={routes} />
+		</Provider>
 	</React.StrictMode>
 );
